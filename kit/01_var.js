41acd805d57aed2a6b1ce80ae8b770ab8e1ba22e
Vishal7683-0 <Karishmampi77@gmail.com>
2025-08-26 15:14:42 +0000

    ok2

---
+++ kit/01_var.js
@@ -0,0 +1,12 @@
+const accountId = 1423
+let accountEmail = "Hdc@.com"
+var accountPassword ="1234"
+accountCity = "Jaipur"
+// accountId = 1234// not allowed in const repeat give value in same conts 
+accountEmail = 'hd.com'
+accountPassword = "12"
+accountCity = 'mumbai'
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+ 
---
+++ kit/02_data.js
@@ -0,0 +1,20 @@
+"use strict"; //treat as a js newer version
+let age = 18
+let isloggedIn = false
+//alert(3+3)// we are use nodejs, not browser . use in chrome console
+
+ //primitive datatypes//
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean = true/false
+//null = standalone value like empty
+//undefined => declare variable but not give value
+//SYMBOL=> uniqe
+
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined);
+
+
