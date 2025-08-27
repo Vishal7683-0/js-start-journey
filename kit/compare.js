986dcec43dc13018a31ebeb32504108b8ffcf287
Vishal7683-0 <Karishmampi77@gmail.com>
2025-08-27 07:29:13 +0000

    comparison

---
+++ kit/compare.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1); all comparison false or true but next
+//console.log("2" > 1); //its automatic "2" convert number
+// console.log(null > 0);false
+// console.log(null < 0);false
+// console.log(null == 0); //equality check
+// console.log(null >= 0); true
+//undefined
+console.log(undefined < 0);//false
+console.log(undefined == 0); //equality check
+console.log(undefined >= 0);//false
+//===
+console.log("2" === 2); // its check also check type and give false its accuracy give results
+
+
+
+
+
+
+
+
--- kit/convers.js
+++ kit/convers.js
@@ -9,7 +9,42 @@
 //"vishal" => true
 let someNumber = 33
 let valueIn = String(someNumber)
-console.log(typeof valueIn);
-console.log(valueIn);
+// console.log(typeof valueIn);
+// console.log(valueIn)
+let value = 3
+let negValue =-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+let str1 = "hello"
+let str2 = " Vishal"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // first come string so all value add in string
+// console.log(1 + 2 + "2");// first come Number so first add number and add string
+// console.log((1 + 3) * 4 % 3);// use always parenthis () its easily learn and give accuracy
+// console.log(+true); //its give value 1 but aboid nhi use krna chaiye
+let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2; all num hold value 4 but its hume is method nhi dena chaiye
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
 
 
