32204716c63dab9ae9cdfa70941b946c14c32bfe
Vishal7683-0 <Karishmampi77@gmail.com>
2025-08-27 06:29:28 +0000

    conversion datatypes

---
+++ kit/convers.js
@@ -0,0 +1,15 @@
+//let score = "33abc" //its convert in number but type of Nan its not a number 
+//let score = null // its give value 0
+//let score = "vishal" // its not give value Nan but convert in number
+//let score = true // its give value 1 same as false 0
+//console.log(typeof score);
+// convert in boolean
+//let score = 1 give value true or 0 false
+//"" => false
+//"vishal" => true
+let someNumber = 33
+let valueIn = String(someNumber)
+console.log(typeof valueIn);
+console.log(valueIn);
+
+
